64dd3829c5cd2dba0d54f3ed620c91d555c1e622
Cooke, Grant: ABSA (JHB) <Grant.Cooke@barclayscapital.com>
2017-04-26 12:16:37 +0200

    AQA-970 Include some basic queue information

---
+++ Gateway.Web/Models/Home/IndexModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Gateway.Web.Models.Controllers;
+
+namespace Gateway.Web.Models.Home
+{
+    public class IndexModel
+    {
+        public IndexModel()
+        {
+            HistoryStartTime = DateTime.Now.AddDays(-1);
+        }
+
+        public DateTime HistoryStartTime { get; set; }
+
+        public QueueChartModel Queues { get; set; }
+    }
+}
