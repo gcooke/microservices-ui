1e418c9d05e8b529c4d972154f9b5cacc8dcb254
Mooloo, Ushanta: Barclays (JHB) <Ushanta.Mooloo@barclayscapital.com>
2017-06-20 17:31:34 +0200

    no message

---
+++ Gateway.Web/Utils/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Gateway.Web.Utils
+{
+    public static class Constants
+    {
+       public static string RequestPayloadNamespace =
+            "http://schemas.datacontract.org/2004/07/Bagl.Cib.MSF.Contracts.Model.TypedResponse";
+    }
+}
