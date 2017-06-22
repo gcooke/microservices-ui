ec33273377ca65b94cb4f50d88206a8a6979be2e
Mooloo, Ushanta: Barclays (JHB) <Ushanta.Mooloo@barclayscapital.com>
2017-06-22 11:14:41 +0200

    Fix build by removing Humanizer package.

---
+++ Gateway.Web/Utils/TimeSpanExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Gateway.Web.Utils
+{
+    public static class TimeSpanExtensions
+    {
+        public static string Humanize(this TimeSpan date)
+        {
+            return string.Empty;
+        }
+
+        public static string Humanize(this TimeSpan date, int decimals)
+        {
+            return string.Empty;
+        }
+    }
+}
