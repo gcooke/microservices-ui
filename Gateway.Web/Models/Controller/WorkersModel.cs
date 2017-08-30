576d9f7e17e85da2e135f9c70a4278a7992f3827
Mooloo, Ushanta: Barclays (JHB) <Ushanta.Mooloo@barclayscapital.com>
2017-08-30 17:52:11 +0200

    no message

--- Gateway.Web/Models/Controller/WorkersModel.cs
+++ Gateway.Web/Models/Controller/WorkersModel.cs
@@ -18,52 +18,58 @@ namespace Gateway.Web.Models.Controller
         {
             get
             {
-                return WorkerInfos.GroupBy(c => c.Service)
+                return WorkerInfos
+                    .GroupBy(c => c.Service)
                         .Select(c => new ControllerInfo { Controller = c.Key, WorkerInfos = c.ToList() });
             }
         }
 
         public WorkersModel BuildModel(GatewayInfo gatewayInfo)
         {
+            Func<string, string, bool> processIsAlive = (node, pid) =>
+            {
+                return gatewayInfo
+                    .GatewayNodes
+                    .Where(n => string.Equals(n.Node, node, StringComparison.InvariantCultureIgnoreCase))
+                    .Select(p => p.Processes)
+                    .SelectMany(p => p.Process)
+                    .Any(n => string.Equals(n.PID.ToString(), pid, StringComparison.InvariantCultureIgnoreCase));
+            };
+
             foreach (var version in gatewayInfo.Services.SelectMany(s => s.Versions))
             {
                 var workerInfo = new WorkerInfo(version);
+                var isAlive = processIsAlive(workerInfo.Node, workerInfo.Pid);
+                workerInfo.Status = isAlive ? workerInfo.Status : "Missing";
                 WorkerInfos.Add(workerInfo);
-            }
-
-            foreach (var worker in WorkerInfos)
-            {
-                var found = gatewayInfo
-                       .GatewayNodes
-                       .Where(n => string.Equals(n.Node, worker.Node, StringComparison.InvariantCultureIgnoreCase))
-                       .Select(p => p.Processes)
-                       .SelectMany(p => p.Process)
-                       .Any(p => p.PID.ToString() == worker.Pid);
-
-                if (!found)
-                {
-                    worker.Status = "Missing";
-                }
             }
 
             foreach (var gatewayNode in gatewayInfo.GatewayNodes)
             {
-                if (gatewayNode.Processes == null || gatewayNode.Processes.Process == null) continue;
-                foreach (var process in gatewayNode.Processes.Process)
-                {
-                    var found = WorkerInfos.Any(w => w.Pid != process.PID.ToString() || w.Node == gatewayNode.Node);
+                if (gatewayNode.Processes == null ||
+                    gatewayNode.Processes.Process == null) continue;
 
-                    if (!found)
+                var validProcessIds = WorkerInfos
+                    .Where(n => string.Equals(n.Node, gatewayNode.Node, StringComparison.InvariantCultureIgnoreCase))
+                    .Select(p => p.Pid);
+
+                var unknownProcesses = gatewayNode
+                                        .Processes
+                                        .Process
+                                        .Where(p => !validProcessIds.Contains(p.PID.ToString()))
+                                        .ToList();
+
+                foreach (var process in unknownProcesses)
+                {
+                    var workerInfo = new WorkerInfo
                     {
-                        var workerInfo = new WorkerInfo
-                        {
-                            Id = string.Concat(process.Name, "|", process.PID),
-                            Node = gatewayNode.Node,
-                            Service = string.Concat(process.Name, "|", process.PID),
-                            Status = "Orphaned"
-                        };
-                        WorkerInfos.Add(workerInfo);
-                    }
+                        Id = string.Concat(process.Name, "|", process.PID),
+                        Node = gatewayNode.Node,
+                        Service = string.Concat(process.Name, "|", process.PID),
+                        Status = "Orphaned"
+                    };
+
+                    WorkerInfos.Add(workerInfo);
                 }
             }
 
