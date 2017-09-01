00ef5534635d1cd47a67da27dc8982e5b8ae6d05
Mooloo, Ushanta: Barclays (JHB) <Ushanta.Mooloo@barclayscapital.com>
2017-09-01 12:36:39 +0200

    UI changes

--- Gateway.Web/Models/Controller/ConfigurationModel.cs
+++ Gateway.Web/Models/Controller/ConfigurationModel.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Xml.Serialization;
 
 namespace Gateway.Web.Models.Controller
-{
+{ 
     [XmlRoot(ElementName = "Controller")]
     public class ConfigurationModel
     {
