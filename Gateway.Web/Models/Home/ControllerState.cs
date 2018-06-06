0e0e77d8ac53927855ab92011934d037a42b3731
O'Jageer, Rushil: BAGL South Africa <Rushil.O'Jageer@barclayscapital.com>
2018-06-06 10:57:49 +0200

    Added incomplete request count for controller on home screen

--- Gateway.Web/Models/Home/ControllerState.cs
+++ Gateway.Web/Models/Home/ControllerState.cs
@@ -4,9 +4,11 @@ namespace Gateway.Web.Models.Home
 {
     public class ControllerState : StateItem
     {
+        public int IncompleteRequestCount { get; set; }
+
         public ControllerState()
         {
-            
+            IncompleteRequestCount = 0;
         }
     }
 }
