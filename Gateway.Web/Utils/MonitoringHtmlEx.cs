18a9dfe0501aa356c6f3bba42fddb2161a992ce5
Rushil <rushilojageer@gmail.com>
2018-07-12 11:57:58 +0200

    Modifications to the Monitoring report

---
+++ Gateway.Web/Utils/MonitoringHtmlEx.cs
@@ -0,0 +1,27 @@
+﻿using Gateway.Web.Enums;
+
+namespace Gateway.Web.Utils
+{
+    public static class MonitoringHtmlEx
+    {
+        public static string GetStatusColor(this MonitoringStatus status)
+        {
+            if (status == MonitoringStatus.Ok)
+            {
+                return "darkgreen";
+            }
+
+            if (status == MonitoringStatus.Issue)
+            {
+                return "red";
+            }
+
+            if (status == MonitoringStatus.Warning)
+            {
+                return "orange";
+            }
+
+            return string.Empty;
+        }
+    }
+}
