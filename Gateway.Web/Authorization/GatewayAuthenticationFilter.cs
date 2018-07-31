97cc82d3eb600bfcfcc28d5a2f9c61c3d71df3a0
Rushil <rushilojageer@gmail.com>
2018-07-31 16:24:01 +0200

    Changed gateway auth mechanism to use token from cookie

---
+++ Gateway.Web/Authorization/GatewayAuthenticationFilter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Filters;
+using Bagl.Cib.MSF.ClientAPI.Provider;
+
+namespace Gateway.Web.Authorization
+{
+    public class GatewayAuthenticationFilter : IAuthenticationFilter
+    {
+        public void OnAuthentication(AuthenticationContext filterContext)
+        {
+            if (!filterContext.HttpContext.User.Identity.IsAuthenticated)
+            {
+                filterContext.Result = new HttpUnauthorizedResult();
+                return;
+            }
+
+            var controller = (Controller)filterContext.Controller;
+            var authenticationProvider = controller.Resolver.GetService(typeof(IAuthenticationProvider)) as IAuthenticationProvider;
+
+            if (authenticationProvider == null)
+                throw new Exception("Unable to find instance of IAuthenticationProvider.");
+
+            var requestCookie = filterContext.HttpContext.Request.Cookies["X-Token"];
+
+            if (string.IsNullOrWhiteSpace(requestCookie?.Value))
+            {
+                authenticationProvider.Authenticate();
+                var token = authenticationProvider.GetToken();
+
+                var httpCookie = new HttpCookie("X-Token", token) { Expires = DateTime.Now.AddHours(5) };
+                filterContext.HttpContext.Response.Cookies.Add(httpCookie);
+                return;
+            }
+
+            authenticationProvider.SetToken(requestCookie.Value);
+        }
+
+        public void OnAuthenticationChallenge(AuthenticationChallengeContext filterContext)
+        {
+        }
+    }
+}
--- Gateway.Web/Registrations.cs
+++ Gateway.Web/Registrations.cs
@@ -28,7 +28,7 @@ namespace Gateway.Web
             information.RegisterType<IGatewayRestService, GatewayRestService>(Scope.Singleton);
             information.RegisterType<IBasicRestService, BasicRestService>(Scope.Singleton);
             information.RegisterType<IRoleService, RoleService>(Scope.Singleton);
-            information.RegisterType<IAuthenticationProvider, AuthenticationProvider>(Scope.Singleton);
+            information.RegisterType<IAuthenticationProvider, AuthenticationProvider>(Scope.ContainerSingleton);
             information.RegisterType<IDateTimeProvider, DateTimeProvider>(Scope.Singleton);
             information.RegisterType<IRoleService, RoleService>(Scope.Singleton);
             information.RegisterType<IFileService, FileService>(Scope.Singleton);
