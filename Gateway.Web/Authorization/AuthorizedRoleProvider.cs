61cad96b17b150afc61c9b25a9da746fcea5f442
Grant Cooke <Grant.Cooke@barclayscapital.com>
2019-03-01 11:15:56 +0200

    Improve unauthorized access reporting

--- Gateway.Web/Authorization/AuthorizedRoleProvider.cs
+++ Gateway.Web/Authorization/AuthorizedRoleProvider.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Security;
@@ -6,6 +7,7 @@ using Absa.Cib.Authorization.Extensions;
 using Bagl.Cib.MIT.Logging;
 using Bagl.Cib.MSF.ClientAPI.Provider;
 using CommonServiceLocator;
+using Gateway.Web.Models;
 using Unity;
 
 namespace Gateway.Web.Authorization
@@ -22,71 +24,31 @@ namespace Gateway.Web.Authorization
 
         public override bool IsUserInRole(string username, string roleName)
         {
-            var container = (IUnityContainer)HttpContext.Current.Items[MvcApplication.ContainerKey];
-            var authen = container.Resolve<IAuthenticationProvider>();
-            var currenttoken = authen.GetToken();
+            var info = GetUserInformation(_logger);
 
-            if (string.IsNullOrEmpty(currenttoken))
+            if (info.Roles.Count > 0)
             {
-                var cookie = HttpContext.Current.Request.Cookies[GatewayAuthenticationFilter.AuthCockieName];
-
-                if (cookie == null)
-                {
-                    _logger.Info($"No Token Found IsUserInRole for {username}");
-                    return false;
-                }
-
-                authen.SetToken(cookie.Value);
+                return info.Roles.Any(e => e.Equals(roleName, StringComparison.InvariantCultureIgnoreCase));
             }
-               
-            var auhtorize = container.Resolve<IAuthorizationProvider>();
-            var roles = auhtorize.GetClaims<string>(currenttoken, ClaimTypes.Role);
 
-            if (roles != null)
-            {
-                return roles.Where(e => e.Equals(roleName,StringComparison.InvariantCultureIgnoreCase)).Any();
-            }
-            else
-            {
-                _logger.Info($"Checking Right for user : {username} using Token {currenttoken}");
-                _logger.Info($"Roles in Token : {String.Join(",", roles)}");
-                _logger.Error($"User {username} is not in role {roleName}");
-            }
+            _logger.Info($"Checking Right for user : {username} using Token {info.Token}");
+            _logger.Info($"Roles in Token : {string.Join(",", info.Roles)}");
+            _logger.Error($"User {username} is not in role {roleName}");
             return false;
         }
 
         public override string[] GetRolesForUser(string username)
         {
-            var container = (IUnityContainer)HttpContext.Current.Items[MvcApplication.ContainerKey];
-            var authen = container.Resolve<IAuthenticationProvider>();
-            var currenttoken = authen.GetToken();
+            var info = GetUserInformation(_logger);
 
-            if (string.IsNullOrEmpty(currenttoken))
-            {   
-                var cookie = HttpContext.Current.Request.Cookies[GatewayAuthenticationFilter.AuthCockieName];
-
-                if (cookie == null)
-                {
-                    _logger.Info($"No Token Found GetRolesForUser for {username}");
-                    return new string[] { };
-                }
-
-                authen.SetToken(cookie.Value);
-            }
-
-            var auhtorize = container.Resolve<IAuthorizationProvider>();
-            var roles = auhtorize.GetClaims<string>(currenttoken, ClaimTypes.Role);
-            
-            if (roles != null)
+            if (info.Roles.Count > 0)
             {
-                return roles.ToArray();
+                return info.Roles.ToArray();
             }
-            else
-            {
-                _logger.Info($"Checking Right for user : {username} using Token {currenttoken}");
-                _logger.Info($"Roles in Token : {String.Join(",", roles)}");
-                _logger.Error($"User {username} does not have roles defined for Dashboard access.");
-            }
+
+            _logger.Info($"Checking Right for user : {username} using Token {info.Token}");
+            _logger.Info($"Roles in Token : {string.Join(",", info.Roles)}");
+            _logger.Error($"User {username} does not have roles defined for Dashboard access.");
             return new string[] { };
         }
 
@@ -131,5 +93,32 @@ namespace Gateway.Web.Authorization
         }
 
         public override string ApplicationName { get; set; }
+
+        public static AuthorizationModel GetUserInformation(ILogger logger)
+        {
+            var container = HttpContainerProvider.Container;
+            var authen = container.Resolve<IAuthenticationProvider>();
+            var currenttoken = authen.GetToken();
+            var result = new AuthorizationModel();
+
+            if (string.IsNullOrEmpty(currenttoken))
+            {
+                var cookie = HttpContext.Current.Request.Cookies[GatewayAuthenticationFilter.AuthCockieName];
+
+                if (cookie == null)
+                {
+                    logger.Info($"No Token Found");
+                    return result;
+                }
+
+                authen.SetToken(cookie.Value);
+                currenttoken = authen.GetToken();
+            }
+
+            var authorize = container.Resolve<IAuthorizationProvider>();
+            result.Username = authorize.GetClaims<string>(currenttoken, ClaimTypes.Username)?.FirstOrDefault();
+            result.Roles = authorize.GetClaims<string>(currenttoken, ClaimTypes.Role) ?? new List<string>();
+            return result;
+        }
     }
 };
---
+++ Gateway.Web/Authorization/HttpContainerProvider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Web;
+using Unity;
+
+namespace Gateway.Web.Authorization
+{
+    public static class HttpContainerProvider
+    {
+        private static IUnityContainer _container;
+
+        public static IUnityContainer Container
+        {
+            get
+            {
+                if (_container != null)
+                    return _container;
+
+                if (HttpContext.Current == null)
+                    throw new InvalidOperationException("HttpContext not available");
+
+                if (HttpContext.Current.Items[MvcApplication.ContainerKey] == null)
+                    throw new InvalidOperationException("IoC Container not found.");
+
+                return (IUnityContainer)HttpContext.Current.Items[MvcApplication.ContainerKey];
+            }
+        }
+
+
+        public static void SetContainer(IUnityContainer container)
+        {
+            _container = container;
+        }
+    }
+}
