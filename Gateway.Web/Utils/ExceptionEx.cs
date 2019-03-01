d7b3a29011faf0776f2c62460b1e48cd4393bb25
Grant Cooke <Grant.Cooke@barclayscapital.com>
2019-03-01 08:44:08 +0200

    Show inner exception on error page

---
+++ Gateway.Web/Utils/ExceptionEx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gateway.Web.Utils
+{
+    public static class ExceptionEx
+    {
+        public static IEnumerable<Exception> EnumerateExceptions(this Exception ex)
+        {
+            var item = ex;
+            while (item != null)
+            {
+                yield return item;
+                item = item.InnerException;
+            }
+        }
+    }
+}
