831d6508c297f988805644476f06ce44c4941d9b
Theo Willemse <theo.willemse@absa.africa>
2019-05-17 12:15:28 +0200

    Reverted Date range change
    Reverted date range change for schedules per day to reflect the correct status for jobs

--- Gateway.Web/Controllers/ScheduleController.cs
+++ Gateway.Web/Controllers/ScheduleController.cs
@@ -20,11 +20,11 @@ namespace Gateway.Web.Controllers
     public class ScheduleController : BaseController
     {
         private readonly IBatchConfigDataService _batchConfigDataService;
-        private readonly IScheduleDataService _scheduleDataService;
-        private readonly IScheduleService<ScheduleBatchModel> _scheduleBatchService;
-        private readonly IScheduleService<ScheduleWebRequestModel> _scheduleWebRequestService;
-        private readonly IScheduleGroupService _scheduleGroupService;
         private readonly IGateway _gateway;
+        private readonly IScheduleService<ScheduleBatchModel> _scheduleBatchService;
+        private readonly IScheduleDataService _scheduleDataService;
+        private readonly IScheduleGroupService _scheduleGroupService;
+        private readonly IScheduleService<ScheduleWebRequestModel> _scheduleWebRequestService;
 
         public ScheduleController(IScheduleDataService scheduleDataService,
             IBatchConfigDataService batchConfigDataService,
@@ -41,6 +41,132 @@ namespace Gateway.Web.Controllers
             _scheduleWebRequestService = scheduleWebRequestService;
             _scheduleGroupService = scheduleGroupService;
             _gateway = gateway;
+        }
+
+        [HttpGet]
+        public ActionResult CreateGroup(string cron)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _scheduleGroupService.CreateOrUpdate(cron);
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Update");
+            }
+
+            return RedirectToAction("Update");
+        }
+
+        [HttpGet]
+        [Route("Delete/Bulk")]
+        public RedirectToRouteResult DeleteBulk(string items)
+        {
+            var itemList = items.Split(',').Select(long.Parse);
+
+            foreach (var id in itemList)
+            {
+                _scheduleDataService.DeleteSchedule(id);
+            }
+
+            return RedirectToAction("Update");
+        }
+
+        [HttpGet]
+        [Route("Group/{id}/Delete")]
+        public RedirectToRouteResult DeleteGroup(long id)
+        {
+            _scheduleGroupService.Delete(id);
+            return RedirectToAction("Update");
+        }
+
+        [HttpGet]
+        [Route("Group/{id}/Delete/Confirm")]
+        public ActionResult DeleteGroupConfirmation(long id)
+        {
+            var group = _scheduleGroupService.GetGroup(id);
+            var model = new ScheduleDeleteModel
+            {
+                GroupId = id,
+                ItemsForDeletion = new List<string>
+                {
+                    $"Group '{group.FriendlyScheduleDescription}'"
+                }
+            };
+            return View("Delete", model);
+        }
+
+        [HttpGet]
+        [Route("{id}/Delete")]
+        public RedirectToRouteResult DeleteSchedule(long id)
+        {
+            _scheduleDataService.DeleteSchedule(id);
+            return RedirectToAction("Update");
+        }
+
+        [HttpGet]
+        [Route("Delete/Bulk/Confirmation")]
+        public ActionResult DeleteSchedulesBulkConfirmation(string items)
+        {
+            var itemList = items.Split(',').Select(long.Parse).ToList();
+            var schedules = _scheduleDataService.GetScheduleTasks(itemList);
+            var model = new ScheduleDeleteModel
+            {
+                ScheduledIdList = items,
+                ItemsForDeletion = schedules.Select(x => $"{x.Name} ({x.GroupName})").ToList()
+            };
+            return View("Delete", model);
+        }
+
+        [HttpGet]
+        [Route("{id}/Disable")]
+        public ActionResult DisableSchedule(long id)
+        {
+            _scheduleDataService.DisableSchedule(id);
+            return RedirectToAction("Update");
+        }
+
+        [HttpGet]
+        [Route("Status")]
+        public string GetStatus(DateTime? businessDate = null, bool includeDailySummaries = false)
+        {
+            if (businessDate == null)
+                businessDate = DateTime.Now;
+
+            var startDate = businessDate?.AddMinutes(-1);
+            var endDate = startDate?.AddHours(24);
+            var tasks = _scheduleGroupService.GetScheduleGroups(startDate.Value, endDate.Value, null, true);
+            var statuses = tasks
+                .SelectMany(x => x.Tasks)
+                .Select(x => new ScheduleStatus
+                {
+                    GroupId = x.GroupId,
+                    BusinessDate = x.BusinessDate?.ToString("yyyy-MM-dd"),
+                    ScheduleId = x.ScheduleId,
+                    Status = x.Status,
+                    RequestId = x.RequestId,
+                    StartedAt = x.StartedAt?.ToString("HH:mm"),
+                    FinishedAt = x.FinishedAt?.ToString("HH:mm"),
+                    Retries = x.Retries,
+                    TimeTakenFormatted = x.TimeTakenFormatted
+                })
+                .ToList();
+
+            var model = new ScheduleStatusSummary
+            {
+                TaskStatus = statuses,
+                DailySummaries = includeDailySummaries ? _scheduleDataService.GetDailyStatuses(DateTime.Now) : null
+            };
+
+            return JsonConvert.SerializeObject(model);
         }
 
         [HttpGet]
@@ -71,83 +197,10 @@ namespace Gateway.Web.Controllers
         }
 
         [HttpGet]
-        [Route("Update")]
-        public ActionResult Update(string searchTerm = null)
+        [Route("Group/Rerun/{id}/{businessDate}")]
+        public void RerunGroup(long id, DateTime businessDate, string searchTerm)
         {
-            var startDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1).AddMinutes(-1);
-            var endDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month));
-
-            var groups = _scheduleGroupService.GetGroups(startDate, endDate, searchTerm);
-            var model = new ScheduleGroupModel { Groups = groups, SearchTerm = searchTerm };
-
-            foreach (var group in model.Groups)
-            {
-                group.ScheduleBatchModel.Group = group.GroupId.ToString();
-                group.ScheduleWebRequestModel.Group = group.GroupId.ToString();
-                group.ScheduleBatchModel.SetData(_batchConfigDataService);
-            }
-
-            return View(model);
-        }
-
-        [HttpGet]
-        [Route("Group/{id}/Delete/Confirm")]
-        public ActionResult DeleteGroupConfirmation(long id)
-        {
-            var group = _scheduleGroupService.GetGroup(id);
-            var model = new ScheduleDeleteModel
-            {
-                GroupId = id,
-                ItemsForDeletion = new List<string>
-                {
-                    $"Group '{group.FriendlyScheduleDescription}'"
-                }
-            };
-            return View("Delete", model);
-        }
-
-        [HttpGet]
-        [Route("Delete/Bulk/Confirmation")]
-        public ActionResult DeleteSchedulesBulkConfirmation(string items)
-        {
-            var itemList = items.Split(',').Select(long.Parse).ToList();
-            var schedules = _scheduleDataService.GetScheduleTasks(itemList);
-            var model = new ScheduleDeleteModel
-            {
-                ScheduledIdList = items,
-                ItemsForDeletion = schedules.Select(x => $"{x.Name} ({x.GroupName})").ToList()
-            };
-            return View("Delete", model);
-        }
-
-        [HttpGet]
-        [Route("{id}/Delete")]
-        public RedirectToRouteResult DeleteSchedule(long id)
-        {
-            _scheduleDataService.DeleteSchedule(id);
-            return RedirectToAction("Update");
-        }
-
-        [HttpGet]
-        [Route("Delete/Bulk")]
-        public RedirectToRouteResult DeleteBulk(string items)
-        {
-            var itemList = items.Split(',').Select(long.Parse);
-
-            foreach (var id in itemList)
-            {
-                _scheduleDataService.DeleteSchedule(id);
-            }
-
-            return RedirectToAction("Update");
-        }
-
-        [HttpGet]
-        [Route("Group/{id}/Delete")]
-        public RedirectToRouteResult DeleteGroup(long id)
-        {
-            _scheduleGroupService.Delete(id);
-            return RedirectToAction("Update");
+            _scheduleDataService.RerunTaskGroup(id, businessDate, searchTerm);
         }
 
         [HttpGet]
@@ -155,13 +208,6 @@ namespace Gateway.Web.Controllers
         public void RerunTask(long id, DateTime businessDate)
         {
             _scheduleDataService.RerunTask(id, businessDate);
-        }
-
-        [HttpGet]
-        [Route("Stop/{id}/{businessDate}")]
-        public void StopTask(long id, DateTime businessDate)
-        {
-            _scheduleDataService.StopTask(id);
         }
 
         [HttpGet]
@@ -210,13 +256,6 @@ namespace Gateway.Web.Controllers
         }
 
         [HttpGet]
-        [Route("Group/Rerun/{id}/{businessDate}")]
-        public void RerunGroup(long id, DateTime businessDate, string searchTerm)
-        {
-            _scheduleDataService.RerunTaskGroup(id, businessDate, searchTerm);
-        }
-
-        [HttpGet]
         [Route("Group/Stop/{id}/{businessDate}")]
         public void StopGroup(long id, DateTime businessDate, string searchTerm)
         {
@@ -224,69 +263,30 @@ namespace Gateway.Web.Controllers
         }
 
         [HttpGet]
-        [Route("{id}/Disable")]
-        public ActionResult DisableSchedule(long id)
+        [Route("Stop/{id}/{businessDate}")]
+        public void StopTask(long id, DateTime businessDate)
         {
-            _scheduleDataService.DisableSchedule(id);
-            return RedirectToAction("Update");
+            _scheduleDataService.StopTask(id);
         }
 
         [HttpGet]
-        [Route("Status")]
-        public string GetStatus(DateTime? businessDate = null, bool includeDailySummaries = false)
+        [Route("Update")]
+        public ActionResult Update(string searchTerm = null)
         {
-            if (businessDate == null)
-                businessDate = DateTime.Now;
+            var startDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1).AddMinutes(-1);
+            var endDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month));
 
-            var startDate = businessDate?.AddDays(-7);
-            var endDate = DateTime.Now;
-            var tasks = _scheduleGroupService.GetScheduleGroups(startDate.Value, endDate, null, true);
-            var statuses = tasks
-                .SelectMany(x => x.Tasks)
-                .Select(x => new ScheduleStatus
-                {
-                    GroupId = x.GroupId,
-                    BusinessDate = x.BusinessDate?.ToString("yyyy-MM-dd"),
-                    ScheduleId = x.ScheduleId,
-                    Status = x.Status,
-                    RequestId = x.RequestId,
-                    StartedAt = x.StartedAt?.ToString("HH:mm"),
-                    FinishedAt = x.FinishedAt?.ToString("HH:mm"),
-                    Retries = x.Retries,
-                    TimeTakenFormatted = x.TimeTakenFormatted
-                })
-                .ToList();
+            var groups = _scheduleGroupService.GetGroups(startDate, endDate, searchTerm);
+            var model = new ScheduleGroupModel { Groups = groups, SearchTerm = searchTerm };
 
-            var model = new ScheduleStatusSummary
+            foreach (var group in model.Groups)
             {
-                TaskStatus = statuses,
-                DailySummaries = includeDailySummaries ? _scheduleDataService.GetDailyStatuses(DateTime.Now) : null
-            };
-
-            return JsonConvert.SerializeObject(model);
-        }
-
-        [HttpGet]
-        public ActionResult CreateGroup(string cron)
-        {
-            try
-            {
-                if (ModelState.IsValid)
-                {
-                    _scheduleGroupService.CreateOrUpdate(cron);
-                }
-            }
-            catch (Exception ex)
-            {
-                ModelState.AddModelError(string.Empty, ex.Message);
+                group.ScheduleBatchModel.Group = group.GroupId.ToString();
+                group.ScheduleWebRequestModel.Group = group.GroupId.ToString();
+                group.ScheduleBatchModel.SetData(_batchConfigDataService);
             }
 
-            if (!ModelState.IsValid)
-            {
-                return RedirectToAction("Update");
-            }
-
-            return RedirectToAction("Update");
+            return View(model);
         }
     }
 }
