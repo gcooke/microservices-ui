838d0b56311a39c50868a061078c08996812d998
Mooloo, Ushanta: BAGL South Africa <Ushanta.Mooloo@absa.co.za>
2019-07-22 17:13:13 +0200

    SIG-3888

--- Gateway.Web/Models/Pdc/PdcServiceModel.cs
+++ Gateway.Web/Models/Pdc/PdcServiceModel.cs
@@ -1,18 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-
-namespace Gateway.Web.Models.Pdc
+﻿namespace Gateway.Web.Models.Pdc
 {
-    public class PdcServicesModel
-    {
-        public void Load(IEnumerable<PdcServiceModel> items)
-        {
-            Items = items.ToList();
-        }
-
-        public List<PdcServiceModel> Items { get; private set; }
-    }
-
     public class PdcServiceModel
     {
         public PdcServiceModel()
---
+++ Gateway.Web/Models/Pdc/PdcServicesModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Gateway.Web.Models.Pdc
+{
+    public class PdcServicesModel
+    {
+        public void Load(IEnumerable<PdcServiceModel> items)
+        {
+            Items = items.ToList();
+        }
+
+        public List<PdcServiceModel> Items { get; private set; }
+    }
+}
