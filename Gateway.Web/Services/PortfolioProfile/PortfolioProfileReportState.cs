9fe47b3eca9c35465df6b17280b533c235beac04
O'Jageer, Rushil: BAGL South Africa <Rushil.O'Jageer@barclayscapital.com>
2019-09-13 13:50:08 +0200

    Portfolio Profile Monitoring

---
+++ Gateway.Web/Services/PortfolioProfile/PortfolioProfileReportState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Bagl.Cib.MIT.Cube;
+
+namespace Gateway.Web.Services.PortfolioProfile
+{
+    public class PortfolioProfileReportState
+    {
+        public string Portfolio { get; set; }
+        public string Report { get; set; }
+        public string Batch { get; set; }
+        public DateTime? LatestRisksAvailable { get; set; }
+        public DateTime? LastGeneratedAt { get; set; }
+        public string Status { get; set; }
+
+        public void LoadFromRow(IRow row)
+        {
+            Portfolio = row.GetStringValue("Portfolio");
+            Report = row.GetStringValue("Report");
+            Batch = row.GetStringValue("Batch");
+            LatestRisksAvailable = row.GetValue<DateTime>("Latest Risks Available");
+            LastGeneratedAt = row.GetValue<DateTime>("Last Generated At");
+            Status = row.GetStringValue("Status");
+        }
+    }
+}
---
+++ Gateway.Web/Services/PortfolioProfile/PortfolioProfileReportStates.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gateway.Web.Services.PortfolioProfile
+{
+    public class PortfolioProfileReportStates
+    {
+        public string Site { get; set; }
+        public DateTime BusinessDate { get; set; }
+        public IList<PortfolioProfileReportState> States { get; set; }
+
+        public PortfolioProfileReportStates(string site, DateTime businessDate)
+        {
+            Site = site;
+            BusinessDate = businessDate;
+            States = new List<PortfolioProfileReportState>();
+        }
+    }
+}
