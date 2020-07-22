a60d19ac6bb233ce528b34a3a76f61efec57ca1e
Mahon, Trevor: ABSA (JHB) <Trevor.Mahon@barclayscapital.com>
2020-07-22 14:34:59 +0200

    removed unused refrence

--- Gateway.Web/Database/GatewayDatabaseService.cs
+++ Gateway.Web/Database/GatewayDatabaseService.cs
@@ -20,7 +20,6 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Xml.Linq;
-using Bagl.Cib.MIT.StatePublisher.Utils;
 using QueueChartModel = Gateway.Web.Models.Controller.QueueChartModel;
 
 namespace Gateway.Web.Database
